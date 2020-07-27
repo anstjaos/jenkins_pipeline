9078df6b6a0abd70d9449d1ccb0bd5bca172a103
Moonseo Kim <moonseo.kim@nhn.com>
2020-07-27 16:39:43 +0900

    Update maven_build.groovy

--- vars/maven_build.groovy
+++ vars/maven_build.groovy
@@ -2,11 +2,13 @@
 
 def call() {
     node {
+        checkout scm
+        
       stage('withEnv') {
         withEnv(["PATH+MAVEN=${tool 'mvn-3.6.2'}/bin"]) {
             sh script: "mvn clean test -Dmaven.test.failure.ignore=true"
         }
-    }
+      }
     
       stage('report') {
           junit 'target/surefire-reports/*.xml'
